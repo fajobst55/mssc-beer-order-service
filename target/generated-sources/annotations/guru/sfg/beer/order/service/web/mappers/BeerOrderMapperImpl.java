49f07db916f72b99e590b300f01a0beef1395fd8
Frank Jobst <fajobst@gmail.com>
2021-05-22 12:03:04 -0500

    Add HikariCP Config and debug closes #6

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-21T19:46:19-0500",
+    date = "2021-05-22T11:39:09-0500",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.9 (Oracle Corporation)"
 )
 @Component
