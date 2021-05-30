8acee7104f9174167ecc6f227d2d1f020a251edd
Frank Jobst <fajobst@gmail.com>
2021-05-30 18:37:30 -0500

    refactor order states and events. closes #16

--- src/main/java/guru/sfg/beer/order/service/services/sm/BeerOderStateMachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/services/sm/BeerOderStateMachineConfig.java
@@ -30,7 +30,7 @@ public class BeerOderStateMachineConfig extends StateMachineConfigurerAdapter<Be
     @Override
     public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
         transitions.withExternal()
-                .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.NEW)
+                .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_PENDING)
                 .event(BeerOrderEventEnum.VALIDATE_ORDER)
         .and().withExternal()
                 .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED)
