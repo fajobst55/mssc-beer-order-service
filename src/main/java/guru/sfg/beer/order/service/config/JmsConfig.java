b386efa6d06810cf6b91f78c065c7fed7be43dea
Frank Jobst <fajobst@gmail.com>
2021-05-31 19:03:15 -0500

    handle allocation requests. closes #21

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
 @Configuration
-public class JmsConfig {
+public class JmsConfig<ALLOCATE_ORDER_QUEUE> {
 
     public static final String MY_QUEUE = "my-hello-world";
     public static final String MY_SEND_RCV_QUEUE = "replybacktome";
@@ -15,6 +15,7 @@ public class JmsConfig {
     public static final String VALIDATE_ORDER_QUEUE = "validate-order";
     public static final String VALIDATE_ORDER_RESPONSE_QUEUE = "validate-order-response";
     public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
+    public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order-response";
 
     @Bean
     public MessageConverter messageConverter() {
